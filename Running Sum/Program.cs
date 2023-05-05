cd5f80ccd8a26482e823855338ce867a40ac0766
karim ahmed <ka1658115@gmail.com>
2023-05-05 15:43:18 +0300

    commit

---
+++ Running Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Running_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
