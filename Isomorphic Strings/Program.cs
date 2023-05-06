882e3414162f89c58287de769986ec308bd419b7
karim ahmed <ka1658115@gmail.com>
2023-05-06 13:45:05 +0300

    IsIsomorphic

---
+++ Isomorphic Strings/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Isomorphic_Strings
+{
+    internal class Program
+    {
+        /*
+        1 <= s.length <= 5 * 104
+t.length == s.length*/
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length >= 1 || s.Length <= 10000)
+            {
+                if (s.Length == t.Length)
+                {
+                    int[] arr1=new int[s.Length];
+                    int[] Counterarr1 = new int[s.Length] ;
+                    
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        Counterarr1[i] = i;
+                        for (int j = 0; j < s.Length   ; j++)
+                        {
+                            if(j > i)
+                            {
+                                if (s[i] == s[j])
+                                {
+                                    arr1[i] = i;
+                                    arr1[j] = j;
+                                    Counterarr1[i]++;
+                                }
+                            }
+                            
+                        } 
+                    }
+
+                    int[] arr2 = new int[s.Length];
+                    int[] Counterarr2 = new int[s.Length];
+
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        Counterarr2[i] = i;
+
+                        for (int j = 0; j < s.Length; j++)
+                        {
+                            if (j > i)
+                            {
+                                if (t[i] == t[j])
+                                {
+                                    arr2[i] = i;
+                                    arr2[j] = j;
+                                    Counterarr2[i]++;
+                                }
+                            }
+
+                        }
+                    }
+                    bool areEqual = arr1.SequenceEqual(arr2);
+                    bool areEqual2 = Counterarr1.SequenceEqual(Counterarr2);
+                    return areEqual && areEqual2;
+                }
+            }
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            bool x = IsIsomorphic("aaeaa", "uuxyy");
+            Console.WriteLine(x);
+        }
+    }
+}
--- Running Sum/Program.cs
+++ Running Sum/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] sum = new int[nums.Length];
+            if (nums.Length <= 1000 || nums.Length >= 1)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (i <= j)
+                        {
+                            sum[j] += nums[i];
+                        }
+                    }
+                }
+            }
+            return sum;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
