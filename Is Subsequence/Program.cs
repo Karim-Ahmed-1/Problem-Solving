ec7c9e51a6cff278b3ad85e4f5fc87b0aafacc7e
karim ahmed <ka1658115@gmail.com>
2023-05-06 16:15:17 +0300

    Adding problem text

--- Is Subsequence/Program.cs
+++ Is Subsequence/Program.cs
@@ -2,6 +2,61 @@
 {
     internal class Program
     {
+        #region problem
+        /*
+         Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+            A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+            Example 1:
+
+            Input: s = "abc", t = "ahbgdc"
+            Output: true
+            Example 2:
+
+            Input: s = "axc", t = "ahbgdc"
+            Output: false
+ 
+
+            Constraints:
+
+            0 <= s.length <= 100
+            0 <= t.length <= 104
+            s and t consist only of lowercase English letters.
+ 
+
+            Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?*/
+        #endregion
+        public bool IsSubsequence(string s, string t)
+        {
+            int expectedCounter = s.Length;
+            int counter = 0;
+            int foundedAtIndex=-1;
+            bool flag=true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < t.Length; j++)
+                {
+                    if(j>foundedAtIndex)
+                    if (s[i] == t[j])
+                    {
+                        foundedAtIndex = j;
+
+                        counter++;
+                            break;
+                    }
+
+                }
+
+            }
+            if (expectedCounter == counter)
+                return true;
+            else
+                return false;
+            
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
