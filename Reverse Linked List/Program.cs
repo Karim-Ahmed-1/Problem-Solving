50aac6a750c237a230d761f5ff4783cc696ee3e4
karim ahmed <ka1658115@gmail.com>
2023-05-08 00:08:44 +0300

    Reverse Linked List

---
+++ Reverse Linked List/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reverse_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
