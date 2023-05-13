4fb6cabc8650c885e13ea1b761ee282157a2ea3a
karim ahmed <ka1658115@gmail.com>
2023-05-13 13:47:21 +0300

    Longest Palindrome

---
+++ Longest Palindrome/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Longest_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
